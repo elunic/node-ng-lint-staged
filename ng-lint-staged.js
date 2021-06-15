611da7a0ac27c8b5db0c04c5c8fd9062284b5416
whefter <wh@elunic.com>
2021-06-15 07:53:26 +0200

    style: reformat

--- ng-lint-staged.js
+++ ng-lint-staged.js
@@ -2,7 +2,7 @@
 
 // Inspired by: https://github.com/angular/angular-cli/issues/7612#issuecomment-455802617
 
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 
 const script = process.argv[2] || 'lint';
@@ -11,56 +11,41 @@ const scriptArgs = [];
 const fileArgs = [];
 
 let filesSwitch = false;
-for (let i = 3, iz = process.argv.length; i < iz; i++) {
-    const arg = process.argv[i];
+for (const arg of process.argv) {
+  if (arg === '--') {
+    filesSwitch = true;
+    continue;
+  }
 
-    if (arg === '--') {
-        filesSwitch = true;
-        continue;
-    }
-
-    if (filesSwitch) {
-        fileArgs.push(arg);
-    } else {
-        scriptArgs.push(arg);
-    }
+  if (filesSwitch) {
+    fileArgs.push(arg);
+  } else {
+    scriptArgs.push(arg);
+  }
 }
 
-const files = fileArgs
-    .map((file) => {
-        // ng lint's --files argument only works with relative paths
-        // strip cwd and leading path delimiter
-        return `--files="${path.relative(process.cwd(), file)}"`;
-    })
-;
-
-const npm = spawn(
-    'npm',
-    [
-        'run',
-        script,
-        '--',
-        ...scriptArgs,
-        ...files,
-    ],
-    {
-        cwd: process.cwd(),
-        env: process.env,
-        shell: true,
-    }
-);
-
-npm.stdout.on('data', (data) => {
-    console.log(data.toString());
+const files = fileArgs.map(file => {
+  // ng lint's --files argument only works with relative paths
+  // strip cwd and leading path delimiter
+  return `--files="${path.relative(process.cwd(), file)}"`;
+});
+const npm = spawn('npm', ['run', script, '--', ...scriptArgs, ...files], {
+  cwd: process.cwd(),
+  env: process.env,
+  shell: true,
 });
 
-npm.stderr.on('data', (data) => {
-    console.error(data.toString());
+npm.stdout.on('data', data => {
+  console.log(data.toString());
 });
 
-npm.on('close', (code) => {
-    if (code) {
-        console.error(`Child process existed with code ${code}`);
-        process.exit(1);
-    }
+npm.stderr.on('data', data => {
+  console.error(data.toString());
+});
+
+npm.on('close', code => {
+  if (code) {
+    console.error(`Child process existed with code ${code}`);
+    process.exit(1);
+  }
 });
